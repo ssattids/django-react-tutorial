7d57d71efb294d967f46f47e3fee7eafd9ae7866
Salar Satti <salar@verityplatforms.com>
2020-01-20 11:34:53 -0500

    Completed Section 5 of tutorial

--- leadmanager/leads/api.py
+++ leadmanager/leads/api.py
@@ -7,21 +7,23 @@ from .serializers import LeadSerializer
 class LeadViewSet(viewsets.ModelViewSet):
 
     #simple CRUD API for leads
-    queryset = Lead.objects.all()
-    permission_classes = [
-        permissions.AllowAny
-    ]
-    serializer_class = LeadSerializer
-
+    # queryset = Lead.objects.all()
     # permission_classes = [
-    #     permissions.IsAuthenticated,
+    #     permissions.AllowAny
     # ]
-
     # serializer_class = LeadSerializer
 
-    # def get_queryset(self):
-    #     return self.request.user.leads.all()
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
 
-    # def perform_create(self, serializer):
-    #     serializer.save(owner=self.request.user)
+    serializer_class = LeadSerializer
+
+    #get only the leads of that user
+    def get_queryset(self):
+        return self.request.user.leads.all()
+
+    #only save the lead for the owner
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
     
